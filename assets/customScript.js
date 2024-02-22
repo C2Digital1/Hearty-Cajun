34cf95abb06fbe47d21fbcbeb8e49ea72c66cea5
Saad Ullah Sajid <saad.sajid@c2-digital.com>
2024-02-23 00:02:56 +0500

    Update

--- assets/customScript.js
+++ assets/customScript.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     if ($(".flickity-button").length > 0) {
         $(".flickity-button").html(`<svg class="arrowIcon" viewBox="0 0 22 22"><path fill="currentColor" fill-rule="nonzero" d="M16.986 10.514a.688.688 0 010 .972l-3.667 3.667a.687.687 0 11-.972-.973l2.493-2.493H5.5a.688.688 0 010-1.375h9.34L12.347 7.82a.688.688 0 01.972-.972l3.667 3.667z"/></svg>`)
     }
@@ -66,6 +66,10 @@ $(document).ready(function () {
         $(this).next(".accordianContent").slideToggle("200");
     });
 
+    $(".customDropDownBtn").click(function () {
+        $(this).parent("customSortBtnsContainer").toggleClass("activeDropDown");
+    });
+
     // Cart Page Note Text Area code start 
 
     function updateCartNote() {
@@ -88,4 +92,5 @@ $(document).ready(function () {
     $('.dummyTextArea').on('input', updateCartNote);
 
     // Cart Page Note Text Area code end
+
 });
